f12b4bcfe853faed62147bda2f6566f0c6de81ad
Dan Johnson <dan.johnson@sproutsocial.com>
2022-08-03 08:46:43 -0500

    Hbase 2.2.6 on arm

--- config-hbase.sh
+++ config-hbase.sh
@@ -11,7 +11,7 @@ HBASE_DIST="http://archive.apache.org/dist/hbase"
 # Prevent initramfs updates from trying to run grub and lilo.
 export INITRD=no
 export DEBIAN_FRONTEND=noninteractive
-export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-amd64
+export JAVA_HOME=/usr/lib/jvm/java-8-openjdk-arm64
 
 minimal_apt_get_args='-y --no-install-recommends'
 
